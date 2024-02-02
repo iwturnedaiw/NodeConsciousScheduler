94b7d9563e42cab5d38411370d8b7890265be8f7
Shohei Minami <minami.s.ae@m.titech.ac.jp>
2024-02-03 05:51:58 +0900

    Add arrival resources

--- NodeConsciousScheduler/src/nodeconsciousscheduler/Constants.java
+++ NodeConsciousScheduler/src/nodeconsciousscheduler/Constants.java
@@ -86,7 +86,7 @@ public class Constants {
     static String CUMULATIVE_STARTED_JOB_PER_DAY_OUTPUT = "cumulative_started_job_day.out";
     static String CUMULATIVE_STARTED_JOB_PER_HOUR_OUTPUT = "cumulative_started_job_hour.out";
     static String CUMULATIVE_STARTED_JOB_PER_MINUTE_OUTPUT = "cumulative_started_job_minute.out";    
-    
+
     static String WAITING_RESOURCES_PER_DAY_OUTPUT = "waiting_resources_day.out";
     static String WAITING_RESOURCES_PER_HOUR_OUTPUT = "waiting_resources_hour.out";
     static String WAITING_RESOURCES_PER_MINUTE_OUTPUT = "waiting_resources_minute.out";
@@ -94,7 +94,11 @@ public class Constants {
     static String EXECUTING_RESOURCES_PER_DAY_OUTPUT = "executing_resources_day.out";
     static String EXECUTING_RESOURCES_PER_HOUR_OUTPUT = "executing_resources_hour.out";
     static String EXECUTING_RESOURCES_PER_MINUTE_OUTPUT = "executing_resources_minute.out";
-    
+
+    static String ARRIVAL_RESOURCES_PER_DAY_OUTPUT = "arrival_resources_day.out";
+    static String ARRIVAL_RESOURCES_PER_HOUR_OUTPUT = "arrival_resources_hour.out";
+    static String ARRIVAL_RESOURCES_PER_MINUTE_OUTPUT = "arrival_resources_minute.out";  
+
     static String WAITING_MEMORY_RESOURCES_PER_DAY_OUTPUT = "waiting_memory_resources_day.out";
     static String WAITING_MEMORY_RESOURCES_PER_HOUR_OUTPUT = "waiting_memory_resources_hour.out";
     static String WAITING_MEMORY_RESOURCES_PER_MINUTE_OUTPUT = "waiting_memory_resources_minute.out";
@@ -102,7 +106,11 @@ public class Constants {
     static String EXECUTING_MEMORY_RESOURCES_PER_DAY_OUTPUT = "executing_memory_resources_day.out";
     static String EXECUTING_MEMORY_RESOURCES_PER_HOUR_OUTPUT = "executing_memory_resources_hour.out";
     static String EXECUTING_MEMORY_RESOURCES_PER_MINUTE_OUTPUT = "executing_memory_resources_minute.out";
-    
+
+    static String ARRIVAL_MEMORY_RESOURCES_PER_DAY_OUTPUT = "arrival_memory_resources_day.out";
+    static String ARRIVAL_MEMORY_RESOURCES_PER_HOUR_OUTPUT = "arrival_memory_resources_hour.out";
+    static String ARRIVAL_MEMORY_RESOURCES_PER_MINUTE_OUTPUT = "arrival_memory_resources_minute.out";
+
     static String WASTED_RESOURCE_OF_SYSTEM = "wasted_resources_of_system.out";
     
     static int DAY_IN_SECOND = 60 * 60 * 24;
